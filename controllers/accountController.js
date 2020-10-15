24f72bd88eb9e18e449cc66700713acca482e829
Stephen Scholtz <scholtz.gnome@gmail.com>
2020-10-15 12:29:35 +0200

    started creating authentication routes and forms

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -4,8 +4,14 @@ const lessons_get = (req, res) => res.render("account/lessons");
 
 const invoices_get = (req, res) => res.render("account/invoices");
 
+const login_get = (req, res) => res.render("account/login");
+
+const signup_get = (req, res) => res.render("account/signup");
+
 module.exports = {
   account_get,
   lessons_get,
-  invoices_get
+  invoices_get,
+  login_get,
+  signup_get
 }
--- routes/accountRouter.js
+++ routes/accountRouter.js
@@ -5,5 +5,7 @@ const accountController = require("../controllers/accountController");
 accountRouter.get("/", accountController.account_get);
 accountRouter.get("/lessons/", accountController.lessons_get);
 accountRouter.get("/invoices/", accountController.invoices_get);
+accountRouter.get("/login", accountController.login_get);
+accountRouter.get("/signup", accountController.signup_get);
 
 module.exports = accountRouter;
