d843fa24a1300003af2a16dcd8570ac3d1f6f50a
Stephen Scholtz <scholtz.gnome@gmail.com>
2020-10-17 01:17:02 +0200

    changed email ports to less secure

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -69,8 +69,8 @@ const signup_post = async (req, res) => {
 
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_ADDRESS,
         pass: process.env.EMAIL_PASSWORD,
