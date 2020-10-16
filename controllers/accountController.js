6ac8dbb088f465c069b16a706e039f459ea76b8e
Stephen Scholtz <scholtz.gnome@gmail.com>
2020-10-17 00:48:29 +0200

    added secure email ports

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -69,6 +69,8 @@ const signup_post = async (req, res) => {
 
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_ADDRESS,
         pass: process.env.EMAIL_PASSWORD,
