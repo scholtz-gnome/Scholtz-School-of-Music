a7be6426b2bfbda2e5232f26e92eb388451b90a6
Stephen Scholtz <scholtz.gnome@gmail.com>
2020-10-30 00:23:15 +0200

    completed registration process

--- controllers/joinController.js
+++ controllers/joinController.js
@@ -1,4 +1,6 @@
 const Registration = require("../models/Registration");
+const nodemailer = require("nodemailer");
+const ejs = require("ejs");
 
 const join_get = (req, res) => res.render("join");
 
@@ -40,7 +42,7 @@ const join_details_get = async (req, res) => {
 const details_patch = async (req, res) => {
 
   const id = req.params.id;
-  const { age, gender, student, registrationProcess, first, last, email, cell, discipline, lessons, level } = req.body;
+  const { student, discipline, lessons, level, registrationProcess, first, last, email, cell, age, gender, parent_first, parent_last, parent_email, parent_cell } = req.body;
 
   try {
 
@@ -62,14 +64,67 @@ const details_patch = async (req, res) => {
       const registration = await Registration.findByIdAndUpdate(id, { student: { age, gender, first, last, email, cell }, process: 4 });
       res.status(200).json({ redirect: `/join/${registration._id}` });
     }
+      
   }
   catch (err) {
     console.log(err);
   }
 }
 
+const review_patch = async (req, res) => {
+
+  const id = req.params.id;
+
+  try {
+    const registration = await Registration.findByIdAndUpdate(id, req.body);
+
+    const transporter = nodemailer.createTransport({
+      host: "smtpout.secureserver.net",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.EMAIL_ADDRESS,
+        pass: process.env.EMAIL_PASSWORD,
+      }
+    });
+
+    ejs.renderFile("./views/email/registration.ejs", { registration }, (err, data) => {
+      if (err) {
+        console.log(err)
+      } else {
+        transporter.sendMail({
+          from: "info@scholtzschoolofmusic.co.za",
+          to: registration.parent.email,
+          subject: `Email Confirmation for ${registration.student.first} ${registration.student.last}`,
+          html: data
+        }, (err, info) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(info);
+          }
+        });
+      }
+    });
+
+    res.status(200).json({ redirect: `/join/review/${registration._id}` });
+
+  }
+  catch (err) {
+    console.log(err);
+  }
+
+}
+
 const complete_get = async (req, res) => {
-  
+  const id = req.params.id;
+  try {
+    const registration = await Registration.findById(id);
+    res.render("join/completed", { registration });
+  }
+  catch (err) {
+    console.log(err);
+  }
 }
 
 module.exports = {
@@ -77,5 +132,6 @@ module.exports = {
   join_post,
   join_details_get,
   details_patch,
+  review_patch,
   complete_get
 }
