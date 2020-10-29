4cf0d188768439034c5a417a3e5a43f575e0b6b7
Stephen Scholtz <scholtz.gnome@gmail.com>
2020-10-29 10:53:35 +0200

    added ability to upload and download resources

--- routes/resourcesRouter.js
+++ routes/resourcesRouter.js
@@ -2,7 +2,23 @@ const express = require("express");
 const resourcesRouter = express.Router();
 const resourcesController = require("../controllers/resourcesController");
 
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads")
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
+
 resourcesRouter.get("/", resourcesController.resources_get);
 resourcesRouter.get("/custom", resourcesController.custom_get);
+resourcesRouter.get("/upload", resourcesController.upload_get);
+resourcesRouter.post("/upload", upload.single("resource"), resourcesController.upload_post);
+resourcesRouter.post("/download/:id", resourcesController.download_post);
 
 module.exports = resourcesRouter;
